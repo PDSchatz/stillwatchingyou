6c52aa5e9b3dc6aa0580ab6b3380e902dfd7e475
Chris Winn <chris@sixteeneighty.com>
2013-05-29 12:44:16 -0400

    Add select menu

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,18 @@
 //= require jquery.min
 //= require type
 //= require search
+$(document).ready(function() {
+  $("select").each(function(){  
+      $(this).wrap('<div class="selectbox"/>');  
+      $(this).after("<span class='selecttext'></span><span class='select-arrow'></span>");  
+  });
+  
+  $("select").each(function(){  
+      var val = $(this).children("option:selected").text();  
+      $(this).next(".selecttext").text(val);  
+      $(this).change(function(){  
+          var val = $(this).children("option:selected").text();  
+          $(this).next(".selecttext").text(val);  
+      }); 
+  }); 
+})
