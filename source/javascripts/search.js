c70b5cc4c59810a9cc080e1181d4c89120d94b68
Chris Winn <chris@sixteeneighty.com>
2013-06-12 14:48:10 -0400

    Fixes hotmail, gmail, facebook switch. Adds hotmail support

--- source/javascripts/search.js
+++ source/javascripts/search.js
@@ -1,5 +1,6 @@
 $(function() {
   $('.switch a').on('click', function(e) {
+    
     e.preventDefault();
 
     var current = $('#search-media .current span');
@@ -7,19 +8,23 @@ $(function() {
 
     var oldText = current.html();
     current.html(next.html());
-    next.html(oldText);
     
     var vendor = $(this).attr('data-vendor');
+    
+    $(this).attr('data-vendor', oldText.toLowerCase());
+    next.html(oldText);
     
     if(vendor == 'facebook') {
       $('#search-media .content a').each(function() {
         $(this).attr('href', 'https://www.facebook.com/messages/search?action=recent-messages&query=' + $(this).attr('data-query'));
       });
+    } else if(vendor == 'hotmail') {
+      $('#search-media .content a').each(function() {
+        $(this).attr('href', 'http://blu167.mail.live.com/default.aspx?st=' + $(this).attr('data-query'));
+      });
     } else {
       $('#search-media .content a').each(function() {
-        var query = $(this).attr('data-query');
-        
-        //alert(links[vendor][query]);
+        $(this).attr('href', $(this).attr('data-gmail'));
       });
     }
   });
